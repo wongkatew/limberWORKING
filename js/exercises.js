401f14ad400ce1d9032b3fde6d099f060f1a421a
wongkatew <kww006@ucsd.edu>
2017-11-04 13:20:02 -0700

    making exercise page more dynamic I guess

--- js/exercises.js
+++ js/exercises.js
@@ -4,6 +4,7 @@
   of exercises. Additionally contains the JS to add playlists
   to LocalStorage to display.
 */
+
 /* ---- FAKE DATABASES OF VIDEOS -----*/
 
 // abs pain relief
@@ -43,17 +44,20 @@ var glutsPain = [
   {
     "title": "Overhead Leg Extension",
     "video": "images/glutsPain1.mp4",
-    "prev": "images/glutsPain1.gif"
+    "prev": "images/glutsPain1.gif",
+    "index": 0
   },
   {
     "title": "Crossed Straight Leg Stretch",
     "video": "images/glutsPain2.mp4",
-    "prev": "images/glutsPain2.gif"
+    "prev": "images/glutsPain2.gif",
+    "index": 1
   },
   {
     "title": "Crossed Bent Leg Stretch",
     "video": "images/glutsPain3.mp4",
-    "prev": "images/glutsPain3.gif"
+    "prev": "images/glutsPain3.gif",
+    "index": 2
   }
 ]
 // gluteus maximus flexibility
@@ -106,30 +110,3 @@ var glutsStrength = [
 // shoulders
 
 // thighs
-
-
-/* ----- FUNCTIONS FOR EXERCISE PAGES -----*/
-
-// call this function when the apge loads
-// From lab4 code
-/*
-$(document).ready(function() {
-  var source = $("#entry-template").html();
-  var tempalte = Handlebars.compile(source);
-
-  var parentDiv = $("#templatedProjects");
-
-  // start with a simple template
-  // var html = template(glutsPain);
-  // console.log(html);
-  // parentDiv.append(html);
-
-  // now iterate through the glutsPain list and keep appending
-  for (var i = 0; i < glutsPain.length; i++) {
-    var curData = glutsPain[i];
-    var curHTML = template(curData);
-    parentDiv.append(curHtml);
-  }
-
-  // when you first load the page
-}) */
