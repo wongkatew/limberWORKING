d1d71b55d6231df0cf5ae25e016016dbd30aece0
wongkatew <kww006@ucsd.edu>
2017-11-04 15:05:02 -0700

    still making modifications to the exercise page

--- js/exercises.js
+++ js/exercises.js
@@ -45,18 +45,21 @@ var glutsPain = [
     "title": "Overhead Leg Extension",
     "video": "images/glutsPain1.mp4",
     "prev": "images/glutsPain1.gif",
+    "description": "On a yoga mat or carpet, lie down on your back. Then, keeping your legs as straight as possible, bring them toward you until you feel a stretch. You can grab your legs to help.",
     "index": 0
   },
   {
     "title": "Crossed Straight Leg Stretch",
     "video": "images/glutsPain2.mp4",
     "prev": "images/glutsPain2.gif",
+    "description": "Sit down with your legs straight out, one on top of the other. While keeping your back straight, slowly bend your torso forward until you feel a stretch.",
     "index": 1
   },
   {
     "title": "Crossed Bent Leg Stretch",
     "video": "images/glutsPain3.mp4",
     "prev": "images/glutsPain3.gif",
+    "description": "Sit down with both legs bent, where one leg is reached over the other leg. Slowly move your torso to the left, then the right. Repeat with the other leg over the other leg.",
     "index": 2
   }
 ]
